812e56bde07576fc875116cdd9156fb034b12381
milkitails <llypayutu@gmail.com>
2025-06-25 14:24:06 +0300

    Add files via upload

--- PyHexEncoder.py
+++ PyHexEncoder.py
@@ -6,6 +6,6 @@ parser = argparse.ArgumentParser(description='PyHexInterpreter')
 parser.add_argument('file', type=str, help='file')
 args = parser.parse_args()
 
-with open(args.file, "r") as file:
-	with open(f"{os.path.basename(args.file).replace('.py', '')}.bin", "a") as bin_file:
+with open(args.file, "r", encoding="utf-8") as file:
+	with open(f"{os.path.basename(args.file).replace('.py', '')}.bin", "a", encoding="utf-8") as bin_file:
 		bin_file.write(binascii.hexlify(file.read().encode()).decode())
